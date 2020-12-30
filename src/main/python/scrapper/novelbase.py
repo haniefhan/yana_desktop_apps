23025cd5470e5df2686c8c72e538e6d734778dc1
Hanief <haniefhan@gmail.com>
2020-12-30 10:24:08 +0700

    scrapper :: novel base, add for post type request, add header

--- src/main/python/scrapper/novelbase.py
+++ src/main/python/scrapper/novelbase.py
@@ -6,13 +6,33 @@ class Novelbase():
     _user_agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
 
     @classmethod
-    def _get_web_page(self, url, options=dict()):
+    def _get_web_page(self, url, reqtype="get", options=dict()):
         headers = {}
         headers["User-Agent"] = self._user_agent
+
+        if "Accept" in options:
+            headers["Accept"] = options["Accept"]
+
+        if "Accept-Encoding" in options:
+            headers["Accept-Encoding"] = options["Accept-Encoding"]
+
+        if "Content-Type" in options:
+            headers["Content-Type"] = options["Content-Type"]
+
+        if "Origin" in options:
+            headers["Origin"] = options["Origin"]
+        else:
+            headers["Origin"] = self._base_url
 
         if "Referer" in options:
             headers["Referer"] = options["Referer"]
         else:
             headers["Referer"] = self._base_url
 
-        return requests.get(url, headers=headers)
+        if reqtype == "post":
+            body = {}
+            if "post" in options:
+                body = options["post"]
+            return requests.post(url, headers=headers, data=body)
+        else:
+            return requests.get(url, headers=headers)
