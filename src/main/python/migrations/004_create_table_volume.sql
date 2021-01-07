451b9e300b9ca1d80121ef77c8203dc4bd9303bc
Hanief <haniefhan@gmail.com>
2021-01-07 21:37:21 +0700

    migration :: 004 :: wrong create table volume

--- src/main/python/migrations/004_create_table_volume.sql
+++ src/main/python/migrations/004_create_table_volume.sql
@@ -1,7 +1,6 @@
-CREATE TABLE IF NOT EXISTS novel_source(
-    nv_src_id integer PRIMARY KEY,
-    nv_id integer,
-    src_id integer,
-    FOREIGN KEY (nv_id) REFERENCES novel (nv_id),
-    FOREIGN KEY (src_id) REFERENCES source (src_id)
+CREATE TABLE IF NOT EXISTS volume(
+    volume_id integer PRIMARY KEY,
+    volume_name varchar(255) NULL,
+    nv_id integer NOT NULL,
+    FOREIGN KEY (nv_id) REFERENCES novel (nv_id)
 );
